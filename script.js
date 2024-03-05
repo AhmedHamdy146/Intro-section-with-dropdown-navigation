20b079c979e2f79114437a630b5693c961801b00
Ahmed Hamdy <ah.hamd221@gmail.com>
2024-03-05 23:30:22 +0200

    change animation for dropdown menu

--- script.js
+++ script.js
@@ -3,34 +3,37 @@ const close = document.querySelector(".close-menu");
 const open = document.querySelector(".menu-icon");
 for (const dropBtn of dropdown) {
   dropBtn.addEventListener("click", () => {
-    const dropBrnCh = dropBtn.lastElementChild.classList;
-    if (dropBrnCh.contains("dropdown")) {
-      dropBtn.lastElementChild.classList.toggle("open");
-      if (!dropBrnCh.contains("open")) {
-        if (dropBrnCh.contains("dp--1")) {
-          document.querySelector(
-            ".du-image--1"
-          ).src = `./images/icon-arrow-up.svg`;
-        } else {
-          document.querySelector(
-            ".du-image--2"
-          ).src = `./images/icon-arrow-up.svg`;
-        }
+    const lastChild = dropBtn.lastElementChild;
+    lastChild.classList.toggle("co");
+    if (lastChild.classList.contains("dp--1")) {
+      if (!lastChild.classList.contains("co")) {
+        lastChild.style.height = "18.1rem";
+        document.querySelector(".du-image--1").src =
+          "./images/icon-arrow-up.svg";
+        lastChild.style.boxShadow = `0 1rem 3rem 1rem rgba(0, 0, 0, 0.1)`;
       } else {
-        if (dropBrnCh.contains("dp--1")) {
-          document.querySelector(
-            ".du-image--1"
-          ).src = `./images/icon-arrow-down.svg`;
-        } else {
-          document.querySelector(
-            ".du-image--2"
-          ).src = `./images/icon-arrow-down.svg`;
-        }
+        lastChild.style.height = "0";
+        document.querySelector(".du-image--1").src =
+          "./images/icon-arrow-down.svg";
+        lastChild.style.boxShadow = `none`;
+      }
+    } else {
+      if (!lastChild.classList.contains("co")) {
+        lastChild.style.height = "13.6rem";
+        lastChild.style.boxShadow = `0 1rem 3rem 1rem rgba(0, 0, 0, 0.1)`;
+        document.querySelector(".du-image--2").src =
+          "./images/icon-arrow-up.svg";
+      } else {
+        lastChild.style.height = "0";
+        lastChild.style.boxShadow = `none`;
+
+        document.querySelector(".du-image--2").src =
+          "./images/icon-arrow-down.svg";
       }
     }
-    // if(dropBtn)
   });
 }
+// if(dropBtn)
 
 close.addEventListener("click", () => {
   document.querySelector(".navbar").style.right = `-30rem`;
